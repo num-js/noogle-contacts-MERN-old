95281a3bfa52a08f24699f77a6e7bd0191079104
Numan Ahmed <mdnmnahmed@gmail.com>
2021-01-16 12:59:15 +0530

    Show a Specific Contact functionality added

--- client/src/components/contacts/ViewContact.jsx
+++ client/src/components/contacts/ViewContact.jsx
@@ -1,7 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, useParams, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-const ViewContact = () => {
+const ViewContact = (props) => {
+
+    let { id } = useParams();
+    let history = useHistory();
+
+    const [specificContact, setSpecificContact] = useState([]);
+
+    useEffect(() => {
+        if (id != null) {
+            const specificContactData = props.allContacts.filter(singleContact => singleContact._id === id);
+
+            if (specificContactData.length <= 0) {
+                toast.error('Contact not Found');
+                history.push('/');
+            } else {
+                setSpecificContact(specificContactData[0]);
+            }
+        } else {
+            toast.error('Contact not Found');
+            history.push('/');
+        }
+    }, []);
+
     return (
         <div align="center">
             <div className="col-lg-4 col-sm-12 p-0">
@@ -9,7 +32,7 @@ const ViewContact = () => {
                     <div className="card border-0 shadow">
                         <div className="card-header bg-primary text-light">
                             <Link style={{ float: 'right' }} to="/"><span className="btn btn-light btn-sm fa fa-home"></span></Link>
-                            <b>Numan</b>
+                            <b>{specificContact.name}</b>
                         </div>
                         <div className="card-body">
                             <table>
@@ -18,7 +41,7 @@ const ViewContact = () => {
                                         <a href={`tel:${'number'}`} className="btn btn-dark btn-sm"><span className="fa fa-phone"></span></a>
                                     </td>
                                     &nbsp; &nbsp; &nbsp;
-                                    <td>7001959252</td>
+                                    <td>{specificContact.mobile_num}</td>
                                 </tr>
                                 <br />
                                 <tr>
@@ -26,7 +49,7 @@ const ViewContact = () => {
                                         <a href={`mailto:${'email'}`} className="btn btn-dark btn-sm"><span className="fa fa-envelope"></span></a>
                                     </td>
                                     &nbsp; &nbsp; &nbsp;
-                                    <td>nnnn</td>
+                                    <td>{specificContact.email}</td>
                                 </tr>
                                 <br />
                                 <tr>
@@ -34,12 +57,12 @@ const ViewContact = () => {
                                         <a className="btn btn-dark btn-sm"><span className="fa fa-map-marker text-light"></span></a>
                                     </td>
                                     &nbsp; &nbsp; &nbsp;
-                                    <td>Klkta</td>
+                                    <td>{specificContact.address}</td>
                                 </tr>
                             </table>
-                            <hr/>
+                            <hr />
                             <div>
-                                Full Stack
+                            {specificContact.description}
                             </div>
                         </div>
                     </div>
